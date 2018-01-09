3b158759ff4220ffca1a301fb490ad240be8874a
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-09 23:54:37 +0900

    fix: disable coverage to run for a while as it does not look work

--- pkg/run-backend-tests.sh
+++ pkg/run-backend-tests.sh
@@ -12,12 +12,14 @@ curdir=${0%/*}
 topdir=${curdir}/../
 runtest_sh=${curdir}/runtest.sh
 
-bash ${runtest_sh} ${topdir}/anyconfig_*_backend/
+moddir=$(ls -d1 ${topdir}/anyconfig_*_backend/ | head -n 1)
+
+bash ${runtest_sh} ${moddir}
 for f in $(echo ${topdir}/tests/*.py | sort); do
     bash ${runtest_sh} $f
 done
 
-coverage run --source=${topdir}/anyconfig_*_backend/*.py,tests/*.py
-coverage report -m
+#coverage run --source=${moddir},tests/
+#coverage report -m
 
 # vim:sw=4:ts=4:et:
