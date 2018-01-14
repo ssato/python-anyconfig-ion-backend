3557a0ff5377122d1c1eb2f11f9b90d3b67ab10b
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-14 13:31:27 +0900

    change: pass WITH_COVERAGE=1 explicitly on running tests

--- pkg/run-backend-tests.sh
+++ pkg/run-backend-tests.sh
@@ -14,9 +14,9 @@ runtest_sh=${curdir}/runtest.sh
 
 moddir=$(ls -d1 ${topdir}/anyconfig_*_backend/ | head -n 1)
 
-bash ${runtest_sh} ${moddir}
+WITH_COVERAGE=1 bash ${runtest_sh} ${moddir}
 for f in $(echo ${topdir}/tests/*.py | sort); do
-    bash ${runtest_sh} $f
+    WITH_COVERAGE=1 bash ${runtest_sh} $f
 done
 
 #coverage run --source=${moddir},tests/
