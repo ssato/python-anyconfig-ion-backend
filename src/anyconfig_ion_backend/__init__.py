eb971115704a9e0a93ecb7d1a8cc08cb3f0d69c1
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-01 02:26:57 +0900

    fix: relocate and rename to avoid conflicts

---
+++ src/anyconfig_ion_backend/__init__.py
@@ -0,0 +1,8 @@
+"""anyconfig backend to load and dump Amazon Ion
+(https://http://amzn.github.io) data.
+"""
+from __future__ import absolute_import
+from .ion_ import Parser
+
+__version__ = "0.1.0"
+__all__ = ["Parser"]
